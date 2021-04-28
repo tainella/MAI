8646f0e52ce1837a18dc4aedd09e58326d936521
tainella <priklucheniye@mail.ru>
2021-04-29 01:25:42 +0300

    braces and logic

--- 6_FP/lab3/matrix.lisp
+++ 6_FP/lab3/matrix.lisp
@@ -5,19 +5,18 @@
     (values)))
 
 (defun find_max(a ii jj)
-	(let max (aref a 0 0))
-	(loop for i upfrom 0 below ii do
-      (loop for j upfrom 0 below jj do
-      		(if (< max (aref a i j))
-      			setf max (aref a i j)))))
+	(let (max (aref a 0 0)))
+		(loop for i upfrom 0 below ii do
+      		(loop for j upfrom 0 below jj do
+      			(if (< max (aref a i j))
+      				(setf max (aref a i j))))) max)
 
-(defun max_matrix (a) 
-	(let b make-array ’((array-dimensions a) (array-dimensions a 0)))
-	(loop with n = (array-dimension a)
-      for i upfrom 0 below n do
-      (loop with m = (array-dimension a 0)
-      	for j upfrom 0 below m do
-            (psetf (aref b i j) (find_max a j i)))))
+(defun max_matrix (a)
+	(make-array :initial-contents (loop with n = (array-dimension a 0)
+      									for i upfrom 0 below n collect
+      										(loop with m = (array-dimension a 1)
+      											for j upfrom 0 below m collect
+            										(find_max a j i)))))
 
 (defvar a (make-array ’(4 3) 
             :initial-contents
@@ -27,3 +26,4 @@
               (3 2 9))))
 (print-matrix a)
 (max_matrix a)
+
