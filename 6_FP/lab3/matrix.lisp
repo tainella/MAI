cd55e7e93630a3161f43cbc63dc0884cc258fcb7
tainella <priklucheniye@mail.ru>
2021-04-28 17:04:49 +0300

    started 3lab

---
+++ 6_FP/lab3/matrix.lisp
@@ -0,0 +1,31 @@
+(defun print-matrix (matrix &optional (chars 3) stream)
+  (let ((*print-right-margin* (+ 6 (* (1+ chars)
+                                      (array-dimension matrix 1)))))
+    (pprint matrix stream)
+    (values)))
+
+(defun find_max(a i j)
+	(let max (aref a 0 0))
+	(loop with n = (array-dimension a)
+      for i upfrom 0 below n do
+      (loop with m = (array-dimension a 0)
+      	for j upfrom 0 below m do
+      		(if (< max (aref a i j))
+      			setf max (aref a i j)))))
+
+(defun max_matrix (a) 
+	(let b make-array ’((array-dimensions a) (array-dimensions a 0)))
+	(loop with n = (array-dimension a)
+      for i upfrom 0 below n do
+      (loop with m = (array-dimension a 0)
+      	for j upfrom 0 below m do
+            (psetf (aref b i j) (find_max a j i)))))
+
+(defvar a (make-array ’(4 3) 
+            :initial-contents
+            ’((6 7 8) 
+              (1 2 3)
+              (4 5 0)
+              (3 2 9))))
+(print-matrix a)
+(max_matrix a)
