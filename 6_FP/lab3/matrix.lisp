e31200f8587e4bd3c2feec1773c6d1ce569d07be
tainella <priklucheniye@mail.ru>
2021-04-29 13:43:23 +0300

    it is working

--- 6_FP/lab3/matrix.lisp
+++ 6_FP/lab3/matrix.lisp
@@ -5,23 +5,27 @@
     (values)))
 
 (defun find_max(a ii jj)
-	(let (max (aref a 0 0)))
-		(loop for i upfrom 0 below ii do
-      		(loop for j upfrom 0 below jj do
-      			(if (< max (aref a i j))
-      				(setf max (aref a i j))))) max)
+	(let ((max (aref a 0 jj)))
+		(loop for i from 0 to (- ii 1) do
+      		(loop with m = (array-dimension a 1)
+      			for j from jj to (- m 1) do
+      				(if (< max (aref a i j))
+      				(setf max (aref a i j)))))
+    max))
 
 (defun max_matrix (a)
-	(make-array :initial-contents (loop with n = (array-dimension a 0)
-      									for i upfrom 0 below n collect
-      										(loop with m = (array-dimension a 1)
-      											for j upfrom 0 below m collect
-            										(find_max a j i)))))
+	(let ((b a))
+		(loop with n = (array-dimension a 0)
+      	for i upfrom 0 below n do
+      		(loop with m = (array-dimension a 1)
+      		for j upfrom 0 below m do
+            	(setf (aref b i j) (find_max a i j))))
+    b))
 
 (defvar a (make-array ’(4 3) 
             :initial-contents
-            ’((6 7 8) 
-              (1 2 3)
+            ’((3 7 6) 
+              (1 9 3)
               (4 5 0)
               (3 2 9))))
 (print-matrix a)
