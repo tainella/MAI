b62fc30ffa22352a89f160485e1af6e806c0d401
tainella <priklucheniye@mail.ru>
2021-04-29 13:56:55 +0300

    bigger matrix

--- 6_FP/lab3/matrix.lisp
+++ 6_FP/lab3/matrix.lisp
@@ -6,9 +6,9 @@
 
 (defun find_max(a ii jj)
 	(let ((max (aref a 0 jj)))
-		(loop for i from 0 to (- ii 1) do
+		(loop for i upfrom 0 below ii do
       		(loop with m = (array-dimension a 1)
-      			for j from jj to (- m 1) do
+      			for j upfrom jj below m do
       				(if (< max (aref a i j))
       				(setf max (aref a i j)))))
     max))
@@ -22,12 +22,13 @@
             	(setf (aref b i j) (find_max a i j))))
     b))
 
-(defvar a (make-array ’(4 3) 
+(defvar a (make-array ’(5 4) 
             :initial-contents
-            ’((3 7 6) 
-              (1 9 3)
-              (4 5 0)
-              (3 2 9))))
+            ’((3 7 6 5) 
+              (1 9 3 2)
+              (4 5 0 2)
+              (3 2 9 3)
+              (4 6 2 1))))
 (print-matrix a)
-(max_matrix a)
+(print-matrix (max_matrix a))
 
