3613f96a5f5834c318eca62af621a8f5f67f00ba
tainella <priklucheniye@mail.ru>
2021-04-29 15:16:20 +0300

    working version

--- 6_FP/lab3/matrix.lisp
+++ 6_FP/lab3/matrix.lisp
@@ -4,9 +4,24 @@
     (pprint matrix stream)
     (values)))
 
+(defun copy-array (array &key
+                   (element-type (array-element-type array))
+                   (fill-pointer (and (array-has-fill-pointer-p array)
+                                      (fill-pointer array)))
+                   (adjustable (adjustable-array-p array)))
+  (let* ((dimensions (array-dimensions array))
+         (new-array (make-array dimensions
+                                :element-type element-type
+                                :adjustable adjustable
+                                :fill-pointer fill-pointer)))
+    (dotimes (i (array-total-size array))
+      (setf (row-major-aref new-array i)
+            (row-major-aref array i)))
+    new-array))
+
 (defun find_max(a ii jj)
 	(let ((max (aref a 0 jj)))
-		(loop for i upfrom 0 below ii do
+		(loop for i upfrom 0 below (+ 1 ii) do
       		(loop with m = (array-dimension a 1)
       			for j upfrom jj below m do
       				(if (< max (aref a i j))
@@ -14,7 +29,7 @@
     max))
 
 (defun max_matrix (a)
-	(let ((b a))
+	(let ((b (make-array ((array-dimension a 0) (array-dimension a 1)) : initial-element 0)))
 		(loop with n = (array-dimension a 0)
       	for i upfrom 0 below n do
       		(loop with m = (array-dimension a 1)
@@ -24,11 +39,12 @@
 
 (defvar a (make-array ’(5 4) 
             :initial-contents
-            ’((3 7 6 5) 
-              (1 9 3 2)
-              (4 5 0 2)
+            ’((3 4 6 5) 
+              (1 3 3 2)
+              (4 8 0 2)
               (3 2 9 3)
               (4 6 2 1))))
 (print-matrix a)
 (print-matrix (max_matrix a))
+(print-matrix a)
 
