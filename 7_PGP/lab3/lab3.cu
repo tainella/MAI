0cb08766affa40346d36a492426106700f58f0cf
tainella <priklucheniye@mail.ru>
2021-10-14 12:20:05 +0300

    compilation without errors

--- 7_PGP/lab3/lab3.cu
+++ 7_PGP/lab3/lab3.cu
@@ -22,7 +22,11 @@ __host__ double dist(double x1, double y1, double z1, double x2, double y2, doub
     return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 }
 
-__constant__ pair u[];
+__device__ double dist1(double x1, double y1, double z1, double x2, double y2, double z2) {
+    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+}
+
+__constant__ pair u[32];
 
 __global__ void kernel(uchar4 *data, int w, int h, int n) {
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -30,14 +34,13 @@ __global__ void kernel(uchar4 *data, int w, int h, int n) {
 	int offsetx = blockDim.x * gridDim.x;
 	int offsety = blockDim.y * gridDim.y;
 	int x, y;
-	
+	double mas[32];
     for (y = idy; y < h; y += offsety) {
-        for(x = idx; k < w; x < w; x += offsetx) {
-            double mas[n];
-            double4 p = data[y * w + x]; //int4
+        for(x = idx; x < w; x += offsetx) {
+            double4 p = make_double4(data[y * w + x].x, data[y * w + x].y, data[y * w + x].z, 0);//int4
             for (int j = 0; j < n; j++) {
-                double4 c = data[u[j].y * w + u[j].x];
-                mas[j] = dist(c.x, c.y, c.z, p.x, p.y, p.z); //заполняем массив расстояний            
+                 double4 c = make_double4(data[int(u[j].y) * w + int(u[j].x)].x, data[int(u[j].y) * w + int(u[j].x)].y, data[int(u[j].y) * w + int(u[j].x)].z, 0); //int4
+                 mas[j] = dist1(c.x, c.y, c.z, p.x, p.y, p.z); //заполняем массив расстояний            
             }
             //поиск минимума
             double MIN = mas[0];
@@ -49,7 +52,7 @@ __global__ void kernel(uchar4 *data, int w, int h, int n) {
                 }
             }
             p.w = min_i;
-            data[y*w+x] = p;
+            data[y*w+x] = make_uchar4(p.x, p.y, p.z, p.w);
         }
     }         
 }
@@ -57,7 +60,8 @@ __global__ void kernel(uchar4 *data, int w, int h, int n) {
 int main() {
 	std::string inputFile;
 	std::string outputFile;
-	int n, x, y;
+	int n, w, h;
+    double x, y;
 
 	std::cin >> inputFile >> outputFile;
 	scanf("%d", &n);
@@ -83,21 +87,22 @@ int main() {
 
     bool flag = true;
     while(flag) {
-        CSC(cudaMemcpyToSymbol(u, centre, sizeof(pair) * количество, 0, cudaMemcpyHostToDevice));
+        CSC(cudaMemcpyToSymbol(u, centre, sizeof(pair) * n, 0, cudaMemcpyHostToDevice));
         kernel<<<dim3(16, 16), dim3(16, 16)>>>(dev_data, w, h, n); 
         CSC(cudaMemcpy(data, dev_data, sizeof(uchar4) * h * w, cudaMemcpyDeviceToHost));
        
         //обновление центра кластеров
-        long long int k[n] = 0;
+        long long int k[n];
         long long int sum[n][2];
         for(int y = 0; y < n; y++) {
+            k[y] = 0;
             sum[y][0] = 0;
             sum[y][1] = 0;
         }
         int4 t;
         for(int y = 0; y < h; y++) {
             for(int x = 0; x < w; x++) {
-                t = data[y*w+x];
+                t = make_int4(data[y*w+x].x, data[y*w+x].y, data[y*w+x].z, data[y*w+x].w);
                 k[t.w] += 1;
                 sum[t.w][0] = y;
                 sum[t.w][1] = x;
@@ -136,7 +141,6 @@ int main() {
 	fclose(fp);
 
 	free(data);
-	free(data_out);
+    CSC(cudaFree(dev_data));
 	return 0;
 }
-
