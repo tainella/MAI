3fa0520225ee0339950ac6a203b0a4e8f7f11e1a
tainella <priklucheniye@mail.ru>
2021-10-15 15:36:03 +0300

    till 20 test

--- 7_PGP/lab3/lab3.cu
+++ 7_PGP/lab3/lab3.cu
@@ -16,13 +16,10 @@ do { \
 typedef struct pair {
     double x;
     double y;
+    double z;
 } pair;
 
-__host__ double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
-    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
-}
-
-__device__ double dist1(double x1, double y1, double z1, double x2, double y2, double z2) {
+__device__ double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
     return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 }
 
@@ -39,20 +36,21 @@ __global__ void kernel(uchar4 *data, int w, int h, int n) {
         for(x = idx; x < w; x += offsetx) {
             double4 p = make_double4(data[y * w + x].x, data[y * w + x].y, data[y * w + x].z, 0);//int4
             for (int j = 0; j < n; j++) {
-                 double4 c = make_double4(data[int(u[j].y) * w + int(u[j].x)].x, data[int(u[j].y) * w + int(u[j].x)].y, data[int(u[j].y) * w + int(u[j].x)].z, 0); //int4
-                 mas[j] = dist1(c.x, c.y, c.z, p.x, p.y, p.z); //заполняем массив расстояний            
+                 //int c_i = u[j].y * w + u[j].x;
+                 //double4 c = make_double4(data[c_i].x, data[c_i].y, data[c_i].z, 0); //int4
+                 mas[j] = dist(u[j].x, u[j].y, u[j].z, p.x, p.y, p.z); //заполняем массив расстояний            
             }
             //поиск минимума
             double MIN = mas[0];
             int min_i = 0;
-            for (int j = 0; j < n; j++) {
+            for (int j = 1; j < n; j++) {
                 if (mas[j] < MIN) {
                     MIN = mas[j];
                     min_i = j; 
                 }
             }
-            p.w = min_i;
-            data[y*w+x] = make_uchar4(p.x, p.y, p.z, p.w);
+            data[y*w+x].w = min_i;
+            //printf("%d ", min_i);
         }
     }         
 }
@@ -61,17 +59,12 @@ int main() {
 	std::string inputFile;
 	std::string outputFile;
 	int n, w, h;
-    double x, y;
+    int x, y;
 
 	std::cin >> inputFile >> outputFile;
 	scanf("%d", &n);
     pair centre[n];
     pair centre_new[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%lf %lf", &x, &y);
-        centre[i].x = x;
-        centre[i].y = y;
-    }
 
 	FILE* fp = fopen(inputFile.c_str(), "rb");
 	
@@ -81,52 +74,67 @@ int main() {
 	fread(data, sizeof(uchar4), w * h, fp);
 	fclose(fp);
 
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", &x, &y);
+        double3 g = make_double3(data[y*w+x].x, data[y*w+x].y, data[y*w+x].z);
+        centre[i].x = g.x;
+        centre[i].y = g.y;
+        centre[i].z = g.z;
+    }
+
     uchar4 *dev_data;
     CSC(cudaMalloc(&dev_data, sizeof(uchar4) * h * w));
     CSC(cudaMemcpy(dev_data, data, sizeof(uchar4) * h * w, cudaMemcpyHostToDevice));
 
     bool flag = true;
     while(flag) {
-        CSC(cudaMemcpyToSymbol(u, centre, sizeof(pair) * n, 0, cudaMemcpyHostToDevice));
+        CSC(cudaMemcpyToSymbol(u, centre, sizeof(pair) * n));
         kernel<<<dim3(16, 16), dim3(16, 16)>>>(dev_data, w, h, n); 
+        //printf("\n");
         CSC(cudaMemcpy(data, dev_data, sizeof(uchar4) * h * w, cudaMemcpyDeviceToHost));
        
         //обновление центра кластеров
         long long int k[n];
-        long long int sum[n][2];
+        long long int sum[n][3];
         for(int y = 0; y < n; y++) {
             k[y] = 0;
             sum[y][0] = 0;
             sum[y][1] = 0;
+            sum[y][2] = 0;
         }
-        int4 t;
+        int t;
         for(int y = 0; y < h; y++) {
             for(int x = 0; x < w; x++) {
-                t = make_int4(data[y*w+x].x, data[y*w+x].y, data[y*w+x].z, data[y*w+x].w);
-                k[t.w] += 1;
-                sum[t.w][0] = y;
-                sum[t.w][1] = x;
+                t = data[y*w+x].w;
+                //printf("%d ", t);
+                k[t] += 1;
+                sum[t][0] += data[y*w+x].x;
+                sum[t][1] += data[y*w+x].y;
+                sum[t][2] += data[y*w+x].z;
             }
         }
+        //printf("\n");
         for(int r = 0; r < n; r++) {
-            centre_new[r].x = sum[r][1] / k[r];
-            centre_new[r].y = sum[r][0] / k[r];
+            if (k[r] > 0) {
+                centre_new[r].x = sum[r][0] / k[r];
+                centre_new[r].y = sum[r][1] / k[r];
+                centre_new[r].z = sum[r][2] / k[r];
+            }
         }
         
         //условие прекращения
         for(int i = 0; i < n; i++) {
-            if (centre[i].x == centre_new[i].x && centre[i].y == centre_new[i].y) {
-                //проверяем дальше
-            }
-            else {
+            if (!(centre[i].x == centre_new[i].x && centre[i].y == centre_new[i].y && centre[i].z == centre_new[i].z)) {
                 flag = false; //были не одинаковые
-            }    
+                break;
+            }  
         }
         if (flag == false) {
             flag = true; //продолжаем обработку
             for(int i = 0; i < n; i++) { 
                 centre[i].x = centre_new[i].x;
                 centre[i].y = centre_new[i].y;
+                centre[i].z = centre_new[i].z;
             }
         }
         else {
