54ffb65f2aafc878abe0627919ea4faf3e9c080d
tainella <priklucheniye@mail.ru>
2022-03-10 16:31:38 +0300

    bronnikov style

--- 7_PGP/lab7/mpi.cpp
+++ 7_PGP/lab7/mpi.cpp
@@ -1,252 +1,345 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <stdlib.h>
+#include <iomanip>
+#include <string.h>
 #include <mpi.h>
 
-#define CSC(call)  \
-do { \
-    cudaError_t state = call; \
-    if (state != cudaSuccess) { \
-        fprintf(stderr, "ERROR: %s:%d. Message: %s\n", __FILE__,__LINE__,cudaGetErrorString(state)); \
-        exit(0); \
-    } \
-} while (0); \
+#define RELEASE
 
-#define _i(i, j, j_size) ((j + 1) * (j_size+2) + i + 1)
-
-#define _ib(i, j, b_size) (j * b_size + i)
-
-//по порядку ввода
-#define down 0
-#define up 1
-#define left 2
-#define right 3
-#define front 4
-#define back 5
-
-void tranfer(double** d_in, double** d_out, int* data_sizes, int* coords, int* shape, int* neighb_ranks, MPI_Comm comm){
-    MPI_Request in[6], out[6]; // statuses of exchange beetwen processes
-    //отправка
-    for(int dir = 0; dir < 3; dir++){
-        int dd= dir << 1;
-        if(coords[dir]){
-            MPI_Isend(d_out[dd], data_sizes[dir], MPI_DOUBLE, neighb_ranks[dd], 0, comm, &out[dd]);
-
-            MPI_Irecv(d_in[dd], data_sizes[dir], MPI_DOUBLE, neighb_ranks[dd], 0, comm, &in[dd]);
-        }
-        if(coords[dir] < shape[dir] - 1){
-            MPI_Isend(d_out[dd + 1], data_sizes[dir], MPI_DOUBLE, neighb_ranks[dd + 1], 0, comm, &out[dd + 1]);
-            
-            MPI_Irecv(d_in[dd + 1], data_sizes[dir], MPI_DOUBLE, neighb_ranks[dd + 1], 0, comm, &in[dd + 1]);
-        }
-    }
-
-    // ожидание
-    for(int dir = 0; dir < 3; dir++){
-        int dd = dir << 1;
-        if(coords[dir] > 0){
-            MPI_Status temp;
-            MPI_Wait(&in[dd], &temp);
-            MPI_Wait(&out[dd], &temp);
-        }
-        if(coords[dir] < shape[dir] - 1){
-            MPI_Status temp;
-            MPI_Wait(&in[dd+1], &temp);
-            MPI_Wait(&out[dd+1], &temp);
-        }
-    }
+int is_main(int worker){
+    return worker ? 0 : 1;
 }
 
-void count(double* buf, double** data, int* data_sizes, int* block_size){
-    int start[3];
-    int mu[3];
-    int lm[3];
-    int f[3];
-
-    for(int i = 0; i < 3; i++) { // инициализация
-        f[i] = block_size[i] + 2;
-        lm[i] = block_size[i] + 1;
-        mu[i] = 1;
-        start[i] = 1;
-    }
-
-    for(int dd = 0; dd < 6; dd++){
-        int dir = dd >> 1;
-        start[dir] = (block_size[dir] + 1) * (dd & 1);
-        lm[dir] = (dd & 1) ? (block_size[dir] + 2) : 1;
-        f[dir] = 1;
-        mu[dir] = 0;
-        for(int k = start[2]; k < lm[2]; ++k){
-            for(int j = start[1]; j < lm[1]; ++j){
-                for(int i = start[0]; i < lm[0]; ++i){
-                    buf[i + (j + k * (block_size[1] + 2)) * (block_size[0] + 2)] =
-                        data[dd][i * mu[0] + (j * mu[1] + k * mu[2] * f[1]) * f[0]];
-                }
-            }
-        }
-        f[dir] = lm[dir]--;
-        start[dir] = mu[dir] = 1;
-    }
-}
-
-double max_det(double v1, double v2, double curr){
-    double diff = v1 - v2;
-
-    diff = diff < 0.0 ? -diff : diff;
-    return diff > curr ? diff : curr;
-}
-
-double u_new(double u_x0, double u_x1, double u_y0, double u_y1, double u_z0, double u_z1, double h2x, double h2y, double h2z) {
-    double ans = (u_x0 + u_x1) * h2x;
-    ans += (u_y0 + u_y1) * h2y;
-    ans += (u_z0 + u_z1) * h2z;
+double u_next(double ux0, double ux1, double uy0, double uy1, double uz0, double uz1, double h2x, double h2y, double h2z) {
+    double ans = (ux0 + ux1) * h2x;
+    ans += (uy0 + uy1) * h2y;
+    ans += (uz0 + uz1) * h2z;
     return ans;
 }
 
-int main(int argc, char *argv[]) {
-    //входные данные
-    int shape[3];
-    int block_size[3];
-    std::string out_name;
-    double eps;
-    double l_size[3];
-    double u[6];
-    double u0;
+double max_determine(double val1, double val2, double curr_max) {
+    double diff = val1 - val2;
+    diff = diff < 0.0 ? -diff : diff;
 
-    //данные для работы
-    int id, numproc;
+    return diff > curr_max ? diff : curr_max;
+}
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numproc);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    fprintf(stderr, "%d(%d)\n", id, numproc);
+void recv_waiting(MPI_Request* in, MPI_Request* out){
+    MPI_Status temp;
+    MPI_Wait(in, &temp);
+    MPI_Wait(out, &temp);
+}
 
-    MPI_Barrier(MPI_COMM_WORLD); //синхронизация
-
-    if (id == 0) { //считывает первый процесс
-        std::cin >> shape[0] >> shape[1] >> shape[2];
-        std::cin >> block_size[0] >> block_size[1] >> block_size[2];
-        std::cin >> out_name;
-        std::cin >> eps;
-        std::cin >> l_size[0] >> l_size[1] >> l_size[2];
-        for (int i = 0; i < 6; i++) {
-            std::cin >> u[i];
-        }
-        std::cin >> u0;
+void print_line(std::ostream& os, double* line, int size){
+    for(int i = 0; i < size; ++i){
+        os << line[i] << " ";
     }
+}
 
-    //пересылаем всем другим процессам данные
-    MPI_Bcast(shape, 3, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(block_size, 3, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&eps, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(l_size, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(u, 6, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&u0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+void import_edges(double* inner_buff, double** edge_buffs, int* edge_sizes, int* blocks){
+    int size_x = blocks[0] + 2;
+    int size_y = blocks[1] + 2;
+    int size_z = blocks[2] + 2;
 
-    double hx, hy, hz;
-    hx = l_size[0] / ((double)shape[0] * block_size[0]);
-    hy = l_size[1] / ((double)shape[1] * block_size[1]);
-    hz = l_size[2] / ((double)shape[2] * block_size[2]);
+    int start[3];
+    int m[3];
+    int lims[3];
+    int ones[3];
 
-    double* data[3];
-    int data_sizes[3];
-    double* next[3];
-    data[0] = (double*)malloc(sizeof(double) * (shape[1] + 2) * (shape[2] + 2)); // y z
-    next[0] = (double*)malloc(sizeof(double) * (shape[1] + 2) * (shape[2] + 2));
-    data_sizes[0] = (shape[1] + 2) * (shape[2] + 2);
+    lims[0] = ones[0] = size_x;
+    lims[1] = ones[1] = size_y;
+    lims[2] = ones[2] = size_z;
+    --lims[0]; --lims[1]; --lims[2];
 
-    data[1] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[2] + 2)); // x z
-    next[1] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[2] + 2));
-    data_sizes[1] = (shape[0] + 2) * (shape[2] + 2);
+    m[0] = m[1] = m[2] = 1;
+    start[0] = start[1] = start[2] = 1;
 
-    data[2] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[1] + 2)); // x y
-    next[2] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[1] + 2));
-    data_sizes[2] = (shape[0] + 2) * (shape[1] + 2);
-
-    double* buf[2];
-    buf[0] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[1] + 2) * (shape[2] + 2)); //x y z
-    buf[1] = (double*)malloc(sizeof(double) * (shape[0] + 2) * (shape[1] + 2) * (shape[2] + 2));
-    int bs = (shape[0] + 2) * (shape[1] + 2) * (shape[2] + 2);
-    for (int i = 0; i < bs; i++) {
-        buf[0][i] = u0;
-    }
-
-    //инициализация
-    for (int i = 0; i < shape[1]; i++){
-        for (int j = 0; j < shape[2]; j++){
-            next[0][_i(i, j, shape[2])] = u0; //y z
-        }
-    }
-
-    for (int i = 0; i < shape[0]; i++){
-        for (int j = 0; j < shape[2]; j++){
-            //data[1][_i(i, j, shape[2])] = u0; // x z
-            next[1][_i(i, j, shape[2])] = u0;
-        }
-    }
-    for (int i = 0; i < shape[0]; i++){
-        for (int j = 0; j < shape[1]; j++){
-            //data[2][_i(i, j, shape[1])] = u0; // x y
-            next[2][_i(i, j, shape[1])] = u0;
-        }
-    }
-
-    int period[3], coords[3]; // координаты
-    int neighb_ranks[6];
-    period[0] = 0;
-    period[1] = 0;
-    period[2] = 0;
-    MPI_Comm grid_comm; // коммуникатор
-
-    //топология
-    MPI_Cart_create(MPI_COMM_WORLD, 3, shape, period, false, &grid_comm); //новый коммуникатор для топологии
-    MPI_Comm_rank(grid_comm, &id); //получение нового ранга
-    MPI_Comm_size(grid_comm, &numproc); //получить число процессов
-    MPI_Cart_coords(grid_comm, id, 3, coords); //получить координаты
-
-    //ранги соседей
-    MPI_Cart_shift(grid_comm, 0, 1, &neighb_ranks[left], &neighb_ranks[right]);
-    MPI_Cart_shift(grid_comm, 1, 1, &neighb_ranks[front], &neighb_ranks[back]);
-    MPI_Cart_shift(grid_comm, 2, 1, &neighb_ranks[down], &neighb_ranks[up]);
-
-    //крайние точки
-    for(int dir = 0; dir < 3; ++dir){
-        int dd= dir << 1;
-        if (coords[dir] == 0) {
-            std::fill_n(data[dd], data_sizes[dir], u[dd]);
-        }
-        else if (coords[dir] == shape[dir] - 1) {
-            std::fill_n(data[dd+1], data_sizes[dir], u[dd+1]);
-        }
-    }
-
-    double max_diff = 1;
-    while (max_diff >= eps) {
-        MPI_Barrier(MPI_COMM_WORLD); //синхронизация процессов
-
-        tranfer(data, next, data_sizes, coords, shape, neighb_ranks, grid_comm); //отправка и получение
-        count(buf[0], data, data_sizes, block_size);
-
-        for(int k = 1; k <= block_size[2]; k++){
-            for(int j = 1; j <= block_size[1]; j++){
-                for(int i = 1; i <= block_size[0]; i++){
-                    buf[1][i + (shape[0] + 2)*(j + k*(shape[1] + 2))] = u_new(buf[0][i - 1 + (shape[0] + 2)*(j + k*(shape[1] + 2))], buf[0][i + 1 + (shape[0] + 2)*(j + k*(shape[1] + 2))],buf[0][i + (shape[0] + 2)*(j - 1 + k*(shape[1] + 2))],
-                    buf[0][i + (shape[0] + 2)*(j + 1 + k*(shape[1] + 2))], buf[0][i + (shape[0] + 2)*(j + (k-1)*(shape[1] + 2))], buf[0][i + (shape[0] + 2)*(j + (k+1)*(shape[1] + 2))],
-                    hx, hy, hz);
-                    max_diff = max_det(buf[0][i + (shape[0] + 2)*(j + k*(shape[1] + 2))], buf[1][i + (shape[0] + 2)*(j + k*(shape[1] + 2))], max_diff);
+    for(int orr = 0; orr < 6; ++orr){
+        int dir = orr >> 1;
+        start[dir] = (blocks[dir] + 1) * (orr & 1);
+        lims[dir] = (orr & 1) ? (blocks[dir] + 2) : 1;
+        ones[dir] = 1;
+        m[dir] = 0;
+ 
+        for(int k = start[2]; k < lims[2]; ++k){
+            for(int j = start[1]; j < lims[1]; ++j){
+                for(int i = start[0]; i < lims[0]; ++i){
+                    inner_buff[i + (j + k*size_y)*size_x] =
+                        edge_buffs[orr][i*m[0] + (j*m[1] + k*m[2]*ones[1])*ones[0]];
                 }
             }
         }
 
-        //заполнить перед передачей
+        ones[dir] = lims[dir]--;
+        start[dir] = m[dir] = 1;
+    }
+}
 
+
+void export_edges(double* inner_buff, double** edge_buffs, int* edge_sizes, int* blocks){
+    int size_x = blocks[0] + 2;
+    int size_y = blocks[1] + 2;
+    int size_z = blocks[2] + 2;
+
+    int start[3];
+    int m[3];
+    int lims[3];
+    int ones[3];
+
+    lims[0] = ones[0] = size_x;
+    lims[1] = ones[1] = size_y;
+    lims[2] = ones[2] = size_z;
+    --lims[0];
+    --lims[1];
+    --lims[2];
+
+    m[0] = m[1] = m[2] = 1;
+    start[0] = start[1] = start[2] = 1;
+
+    for(int orr = 0; orr < 6; orr++) {
+        int dir = orr >> 1;
+        if(orr & 1) {
+            start[dir] = blocks[dir];
+        }
+        lims[dir] = (orr & 1) ? blocks[dir] + 1 : 2;
+        ones[dir] = 1;
+        m[dir] = 0;
+ 
+        for(int k = start[2]; k < lims[2]; k++){
+            for(int j = start[1]; j < lims[1]; j++){
+                for(int i = start[0]; i < lims[0]; i++){
+                    edge_buffs[orr][i * m[0] + (j * m[1] + k * m[2] * ones[1]) * ones[0]] = inner_buff[i + (j + k*size_y) * size_x];
+                }
+            }
+        }
+        ones[dir] = lims[dir] + 1;
+        start[dir] = m[dir] = 1;
+    }
+}
+
+
+void edges_exchange(double** edge_buff_in, double** edge_buff_out, int* dir_edge_sizes, int* coords, int* dimens, int* neighb, MPI_Comm grid_comm){
+    MPI_Request in[6];
+    MPI_Request out[6];
+
+    for(int dir = 0; dir < 3; dir++){
+        int dir_x_2 = dir << 1;
+        if(coords[dir]) {
+            MPI_Isend(edge_buff_out[dir_x_2], dir_edge_sizes[dir], MPI_DOUBLE, neighb[dir_x_2], 0, grid_comm, &out[dir_x_2]);
+            MPI_Irecv(edge_buff_in[dir_x_2], dir_edge_sizes[dir], MPI_DOUBLE, neighb[dir_x_2], 0, grid_comm, &in[dir_x_2]);
+        }
+        if(coords[dir] < dimens[dir] - 1) {
+            MPI_Isend(edge_buff_out[dir_x_2 + 1], dir_edge_sizes[dir], MPI_DOUBLE, neighb[dir_x_2 + 1], 0, grid_comm, &out[dir_x_2 + 1]);
+            MPI_Irecv(edge_buff_in[dir_x_2 + 1], dir_edge_sizes[dir], MPI_DOUBLE, neighb[dir_x_2 + 1], 0, grid_comm, &in[dir_x_2 + 1]);
+        }
     }
 
+    for(int dir = 0; dir < 3; dir++){
+        int orr = dir << 1;
+        if(coords[dir] > 0) {
+            recv_waiting(&in[orr], &out[orr]);
+        }
+        if(coords[dir] < dimens[dir] - 1) {
+            recv_waiting(&in[orr + 1], &out[orr + 1]);
+        }
+    }
+}
+
+enum sides {
+    left = 0, right, front, back, down, up
+};
+
+int main(int argc, char **argv) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int main_worker;
+    int proc_rank;
+    int workers_count = 0;
+    
+    int dimens[3], blocks[3];
+    double l[3];
+    double u[6];
+    double u0, eps;
+    std::string path;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    main_worker = is_main(proc_rank);
+
+    if(main_worker) {
+        std::cin >> dimens[0] >> dimens[1] >> dimens[2] >> blocks[0] >> blocks[1] >> blocks[2];
+        std::cin >> path >> eps >> l[0] >> l[1] >> l[2] >> u[down] >> u[up];
+        std::cin >> u[left] >> u[right] >> u[front] >> u[back]>> u0;
+    }
+
+    MPI_Bcast(dimens, 3, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(blocks, 3, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(l, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(u, 6, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&u0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&eps, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    MPI_Comm grid_comm;
+    int pereod[3];
+    int coords[3];
+    int neighb[6];
+
+    std::fill_n(pereod, 3, 0);
+
+    MPI_Cart_create(MPI_COMM_WORLD, 3, dimens, pereod, false, &grid_comm);
+    MPI_Comm_rank(grid_comm, &proc_rank);
+    MPI_Comm_size(grid_comm, &workers_count);
+    MPI_Cart_coords(grid_comm, proc_rank, 3, coords);
+    main_worker = is_main(proc_rank);
+
+    MPI_Cart_shift(grid_comm, 0, 1, &neighb[left], &neighb[right]);
+    MPI_Cart_shift(grid_comm, 1, 1, &neighb[front], &neighb[back]);
+    MPI_Cart_shift(grid_comm, 2, 1, &neighb[down], &neighb[up]);
+
+    double h2x, h2y, h2z;
+    h2x = l[0] / ((double)dimens[0] * blocks[0]);
+    h2y = l[1] / ((double)dimens[1] * blocks[1]);
+    h2z = l[2] / ((double)dimens[2] * blocks[2]);
+
+    h2x *= h2x;
+    h2y *= h2y;
+    h2z *= h2z;
+
+    double denuminator = 2.0*(1.0/h2x + 1.0/h2y + 1.0/h2z);
+    h2x = 1.0 / (denuminator * h2x);
+    h2y = 1.0 / (denuminator * h2y);
+    h2z = 1.0 / (denuminator * h2z);
+
+    int dir_edge_sizes[3];
+    int size_x = blocks[0] + 2;
+    int size_y = blocks[1] + 2;
+    int size_z = blocks[2] + 2;
+
+    dir_edge_sizes[0] = size_y * size_z;
+    dir_edge_sizes[1] = size_x * size_z;
+    dir_edge_sizes[2] = size_y * size_x;
+    
+    double* norm_data = new double[workers_count];
+    double* edge_buff_out[6];
+    double* edge_buff_in[6];
+    double* buff0;
+    double* buff1;
+    
+    int buff_size = size_x * size_y * size_z;
+    buff0 = new double[buff_size];
+    buff1 = new double[buff_size];
+    std::fill_n(buff0, buff_size, u0);
+
+    for(int dir = 0; dir < 3; ++dir) {
+        int dir_x_2 = dir << 1;
+        edge_buff_out[dir_x_2] = new double[dir_edge_sizes[dir]];
+        edge_buff_in[dir_x_2] = new double[dir_edge_sizes[dir]];
+        edge_buff_out[dir_x_2 + 1] = new double[dir_edge_sizes[dir]];
+        edge_buff_in[dir_x_2 + 1] = new double[dir_edge_sizes[dir]];
+
+        std::fill_n(edge_buff_out[dir_x_2], dir_edge_sizes[dir], u0);
+        std::fill_n(edge_buff_out[dir_x_2 + 1], dir_edge_sizes[dir], u0);
+
+        if(!coords[dir]) {
+            std::fill_n(edge_buff_in[dir_x_2], dir_edge_sizes[dir], u[dir_x_2]);
+        }
+        if(coords[dir] == dimens[dir] - 1) {
+            std::fill_n(edge_buff_in[dir_x_2 + 1], dir_edge_sizes[dir], u[dir_x_2 + 1]);
+        }
+    }
+
+    auto idx = [size_x, size_y](int i, int j, int k){
+        return (i + size_x*(j + k*size_y));
+    };
+
+    double max_diff = 0.0;
+
+    do {
+        max_diff = 0.0;
+        edges_exchange(edge_buff_in, edge_buff_out, dir_edge_sizes, coords, dimens, neighb, grid_comm);
+        import_edges(buff0, edge_buff_in, dir_edge_sizes, blocks);
+
+        for(int k = 1; k <= blocks[2]; ++k){
+            for(int j = 1; j <= blocks[1]; ++j){
+                for(int i = 1; i <= blocks[0]; ++i){
+                    buff1[idx(i, j, k)] = u_next(buff0[idx(i - 1, j, k)], buff0[idx(i + 1, j, k)], buff0[idx(i, j - 1, k)], buff0[idx(i, j + 1, k)], buff0[idx(i, j, k - 1)], buff0[idx(i, j, k + 1)], h2x, h2y, h2z);
+                    // compute max abs(diff):
+                    max_diff = max_determine(buff0[idx(i, j, k)], buff1[idx(i, j, k)], max_diff);
+                }
+            }
+        }
+        export_edges(buff1, edge_buff_out, dir_edge_sizes, blocks);
+
+        MPI_Allgather(&max_diff, 1, MPI_DOUBLE, norm_data, 1, MPI_DOUBLE, grid_comm);
+        max_diff = 0.0;
+        for(int i = 0; i < workers_count; ++i){
+            max_diff = max_diff < norm_data[i] ? norm_data[i] : max_diff;
+        }
+
+        double* tmp = buff1;
+        buff1 = buff0;
+        buff0 = tmp;
+    } while (max_diff >= eps);
+
+    std::ofstream fout;
+    if (main_worker) {
+        fout.open(path, std::ofstream::out);
+        fout << std::scientific << std::setprecision(7);
+    }
+
+    int t_coord[3];
+    MPI_Status status;
+
+    for(int k = 0; k < dimens[2]; ++k){
+        t_coord[2] = k;
+        for(int kk = 1; kk <= blocks[2]; kk++){
+            for(int j = 0; j < dimens[1]; j++){
+                t_coord[1] = j;
+                for(int jj = 1; jj <= blocks[1]; ++jj){
+                    for(int i = 0; i < dimens[0]; ++i){
+                        t_coord[0] = i;
+                        if (main_worker) {
+                            if(coords[2] == k && coords[1] == j && coords[0] == i) {
+                                print_line(fout, &buff0[idx(1, jj, kk)], blocks[0]);
+                            }
+                            else{
+                                int rank = 0;
+                                MPI_Cart_rank(grid_comm, t_coord, &rank);
+                                MPI_Recv(buff1, blocks[0], MPI_DOUBLE, rank, 0, grid_comm, &status);
+                                print_line(fout, buff1, blocks[0]);
+                            }
+                        }
+                        else{
+                            if(coords[2] == k && coords[1] == j && coords[0] == i){
+                                MPI_Send( &buff0[idx(1, jj, kk)], blocks[0], MPI_DOUBLE, 0, 0, grid_comm);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     MPI_Finalize();
 
-    for (int i = 0; i < 3; i++) {
-        free(data[i]);
+    delete[] norm_data;
+    delete[] buff0;
+    delete[] buff1;
+
+    for(int dir = 0; dir < 3; ++dir) {
+        int dir_x_2 = dir << 1;
+        delete[] edge_buff_out[dir_x_2];
+        delete[] edge_buff_in[dir_x_2];
+        delete[] edge_buff_out[dir_x_2 + 1];
+        delete[] edge_buff_in[dir_x_2 + 1];
     }
 
+    if(main_worker){
+        fout << "\n";
+        fout.close();
+    }
     return 0;
 }
